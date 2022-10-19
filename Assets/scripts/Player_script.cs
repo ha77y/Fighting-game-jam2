77d4723329e71ae9dc03e7bc675e339f46fe447b
ha77y <114589921+ha77y@users.noreply.github.com>
2022-10-19 16:48:49 +0100

    basic character movement and basic scene built

--- Assets/scripts/Player_script.cs
+++ Assets/scripts/Player_script.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Player_script : MonoBehaviour
 {
+    [SerializeField] float speed; 
+    
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,15 @@ public class Player_script : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetAxis("Horizontal") != 0f)
+        {
+            transform.position = new Vector3(transform.position.x + (Input.GetAxis("Horizontal") * speed * Time.deltaTime), transform.position.y);
+        }
+        if (Input.GetAxis("Vertical") != 0f)
+        {
+            transform.position = new Vector3(transform.position.x , transform.position.y + (Input.GetAxis("Vertical") * speed * Time.deltaTime));
+
+        }
+
     }
 }
