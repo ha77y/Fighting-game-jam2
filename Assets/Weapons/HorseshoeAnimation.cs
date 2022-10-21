54156317fa8c486a478657bd984940cb60a21eda
Jake <jakeedwards238@gmail.com>
2022-10-21 22:25:41 +0100

    Added placeholder enemies to parent the guns Added horseshoe and railgun idle animations Added appropriate shoot function timing
    Signed-off-by: Jake <jakeedwards238@gmail.com>

--- Assets/Weapons/HorseshoeAnimation.cs
+++ Assets/Weapons/HorseshoeAnimation.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Xml.Serialization;
 using UnityEngine;
 
-public class RailgunAnimation : MonoBehaviour
+public class HorseshoeAnimation : MonoBehaviour
 {
     // Start is called before the first frame update
     public AnimationClip clip;
@@ -14,31 +14,40 @@ public class RailgunAnimation : MonoBehaviour
     {
         AnimationEvent evnt;
         evnt = new AnimationEvent();
-
-        evnt.time = 0.5f;
+        evnt.time = 0.1f;
         evnt.functionName = "Shoot";
         anim = gameObject.GetComponent(typeof(Animator)) as Animator;
 
         clip = anim.runtimeAnimatorController.animationClips[1];
-        //clip = anim.runtimeAnimatorController.animationClips[0];
+        clip.AddEvent(evnt);
+        evnt.time = 0.3f;
+        clip.AddEvent(evnt);
+        evnt.time = 0.5f;
+        clip.AddEvent(evnt);
+        evnt.time = 0.7f;
+        clip.AddEvent(evnt);
+        evnt.time = 0.9f;
+        clip.AddEvent(evnt);
+        evnt.time = 0.11f;
         clip.AddEvent(evnt);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
         if (this.transform.parent.GetComponent<Enemy>().isshooting)
         {
-            anim.Play("RailgunShoot");
-        } else
+            anim.Play("HorseshoeShoot");
+        }
+        else
         {
-            anim.Play("RailgunIdle");
+            anim.Play("HorseshoeIdle");
         }
 
     }
     public void Shoot()
     {
-        print("Shoot!!");
+        print("HorseshoeShoot!!");
     }
 }
