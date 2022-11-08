a288de430826b55a038223f27de8bc83cc1869db
Jake <jakeedwards238@gmail.com>
2022-11-08 17:59:39 +0000

    deflect now works on a cooldown
    Signed-off-by: Jake <jakeedwards238@gmail.com>

--- Assets/Weapons/PlayerStats.cs
+++ Assets/Weapons/PlayerStats.cs
@@ -2,6 +2,7 @@ using JetBrains.Annotations;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.UIElements;
 using UnityEngine;
 
 public class PlayerStats : MonoBehaviour
@@ -14,6 +15,11 @@ public class PlayerStats : MonoBehaviour
     public Boolean isAttacking = false;
     public Boolean isDeflecting = false;
     public Boolean isDashing = false;
+    public Boolean deflectCooldown = false;
+    public Boolean dashCooldown = false;
+    public int deflectCooldownLength = 10;
+    public int dashCooldownLength = 0;
+    public int deflectDuration = 5;
     public Collider2D deflectCollision;
     public Collider2D attackCollision;
     // Start is called before the first frame update
@@ -34,8 +40,33 @@ public class PlayerStats : MonoBehaviour
         {
             deflectCollision.gameObject.SetActive(false);
             attackCollision.gameObject.SetActive(true);
+        } else
+        {
+            deflectCollision.gameObject.SetActive(false);
+            attackCollision.gameObject.SetActive(false);
+        }
+
+        if ((Input.GetKeyDown(KeyCode.E) | Input.GetKeyDown(KeyCode.Mouse1)) & !deflectCooldown & !isAttacking & !isDashing)
+        {
+            isDeflecting = true;
+            StartCoroutine(Cooldown("isDeflecting", deflectDuration));
+            deflectCooldown = true;
+            StartCoroutine(Cooldown("deflectCooldown", deflectCooldownLength));
         }
     }
+
+    public IEnumerator Cooldown(string boolean, float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        if (boolean == "isDeflecting")
+        {
+            isDeflecting = !isDeflecting;
+        } else if (boolean == "deflectCooldown")
+        {
+            deflectCooldown = !deflectCooldown;
+        }
+    }
+
     public void Damaged(int amount)
     {
         if (invincible) return;
@@ -64,6 +95,7 @@ public class PlayerStats : MonoBehaviour
             yield return new WaitForSeconds(delta);
 
         }
+        sprite.transform.localScale = Vector3.one;
         invincible = false;
     }
     public void GameOver()
