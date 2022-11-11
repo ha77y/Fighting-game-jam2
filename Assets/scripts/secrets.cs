aa2752712d9232fa738ff2868e7fcedff52c4a4f
ha77y <114589921+ha77y@users.noreply.github.com>
2022-11-11 21:31:20 +0000

    Update secrets.cs

--- Assets/scripts/secrets.cs
+++ Assets/scripts/secrets.cs
@@ -7,13 +7,12 @@ public class secrets : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        print("hello");
         transform.GetComponent<TilemapRenderer>().enabled = false;
         //transform.GetComponent<TilemapCollider2D>().enabled = false;
     }
     private void OnTriggerStay2D(Collider2D collision)
     {
-        secretPath.GetComponent<TilemapRenderer>().enabled = false;
+        transform.GetComponent<TilemapRenderer>().enabled = false;
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
