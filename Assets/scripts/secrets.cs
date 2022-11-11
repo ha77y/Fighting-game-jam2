415bd97972742af2a55646cadc9b34eab67bfb61
Jake <jakeedwards238@gmail.com>
2022-11-11 21:30:27 +0000

    fixed secret script
    Signed-off-by: Jake <jakeedwards238@gmail.com>

--- Assets/scripts/secrets.cs
+++ Assets/scripts/secrets.cs
@@ -7,18 +7,15 @@ public class secrets : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        print("hello");
         transform.GetComponent<TilemapRenderer>().enabled = false;
-        //transform.GetComponent<TilemapCollider2D>().enabled = false;
     }
     private void OnTriggerStay2D(Collider2D collision)
     {
-        secretPath.GetComponent<TilemapRenderer>().enabled = false;
+        transform.GetComponent<TilemapRenderer>().enabled = false;
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
         transform.GetComponent<TilemapRenderer>().enabled = true;
-        //transform.GetComponent<TilemapCollider2D>().enabled = true;
     }
 
 }
