5f8ccf5de2260411597d6565127b4144c392ab5e
ha77y <114589921+ha77y@users.noreply.github.com>
2022-11-11 23:58:45 +0000

    Decorations started, level expanded

--- Assets/scripts/secrets.cs
+++ Assets/scripts/secrets.cs
@@ -7,15 +7,24 @@ public class secrets : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        transform.GetComponent<TilemapRenderer>().enabled = false;
+        if (collision.gameObject.tag == "Player")
+        {
+            transform.GetComponent<TilemapRenderer>().enabled = false;
+        }
     }
     private void OnTriggerStay2D(Collider2D collision)
     {
-        transform.GetComponent<TilemapRenderer>().enabled = false;
+        if (collision.gameObject.tag == "Player")
+        {
+            transform.GetComponent<TilemapRenderer>().enabled = false;
+        }
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
-        transform.GetComponent<TilemapRenderer>().enabled = true;
+        if (collision.gameObject.tag == "Player")
+        {
+            transform.GetComponent<TilemapRenderer>().enabled = true;
+        }
     }
 
 }
