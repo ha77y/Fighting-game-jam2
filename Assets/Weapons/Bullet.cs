780024981f124908c89d2b787e441718831c2fe2
Jake <jakeedwards238@gmail.com>
2022-11-12 23:08:32 +0000

    deflected bullets now travel to mouse point rather than parallel to mouse
    Signed-off-by: Jake <jakeedwards238@gmail.com>

--- Assets/Weapons/Bullet.cs
+++ Assets/Weapons/Bullet.cs
@@ -27,12 +27,14 @@ public class Bullet : MonoBehaviour
 
             Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
             Vector2 mousePoint = new Vector2(mouseRay.direction.x + mouseRay.origin.x, mouseRay.direction.y + mouseRay.origin.y);
-            mousePoint = mousePoint - new Vector2(collision.transform.position.x, collision.transform.position.y);
+            //mousePoint = mousePoint - new Vector2(collision.transform.position.x, collision.transform.position.y);
             gameObject.layer = 11;
             Rigidbody2D b = this.transform.GetComponent<Rigidbody2D>();
             Transform t = collision.transform.parent.parent.GetChild(3).GetChild(0).GetChild(0);
+            Vector2 toMouse = (mousePoint - new Vector2(transform.position.x, transform.position.y)).normalized;
             b.velocity = Vector2.zero;
-            b.velocity = t.right * 10f;
+            b.velocity = toMouse.normalized * 10f;
+            //b.velocity = t.right * 10f;
 
             } else if (collision.gameObject.tag == "Player")
         {
